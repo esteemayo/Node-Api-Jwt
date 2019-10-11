f999dc020c43c9fad279b0f7d90d349a3cb8adde
Emmanuel Adebayo <eadebayo15@gmail.com>
2019-10-12 00:45:28 +0100

    Modified routes/customers.js

--- routes/customers.js
+++ routes/customers.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Customer = require('../models/Customer');
 const { validateCustomer } = require('../helpers/auth');
+const auth = require('../middleware/auth');
+const admin = require('../middleware/admin');
 
 // GET INDEX ROUTE
 router.get('/', async (req, res) => {
@@ -23,7 +25,7 @@ router.get('/more-customers', async (req, res) => {
 });
 
 // POST CUSTOMERS ROUTE/LOGIC
-router.post('/', async (req, res) => {
+router.post('/', auth, async (req, res) => {
     const { error } = validateCustomer(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
@@ -55,7 +57,7 @@ router.put('/:id', async (req, res) => {
 });
 
 // DELETE CUSTOMER ROUTE
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', [auth, admin], async (req, res) => {
     const customer = await Customer.findByIdAndDelete(req.params.id);
 
     if (!customer) return res.status(404).send('The customer with the given ID was not found.');
